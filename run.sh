1b651fa21e63e6be3006ae9d9fd38e3ade22accf
yangguang0405 <yangguang0405@live.com>
2017-09-15 21:50:02 +0800

    UPDATE

--- run.sh
+++ run.sh
@@ -4,6 +4,6 @@ ls -l
 export PATH=$PATH:~/node-v8.5.0-linux-x64/bin
 
 mkdir -p "/tmp/Testude 365/agent"
-echo $pwd
+echo $PWD
 
-java -jar "$pwd/agent/versions/1.0.0/pc-agent.jar" "$pwd/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" 0
+java -jar "$PWD/agent/versions/1.0.0/pc-agent.jar" "$PWD/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" 0
