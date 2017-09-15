f2d79f64b3620e5ba266b575625491cadd98cbef
yangguang0405 <yangguang0405@live.com>
2017-09-15 22:00:54 +0800

    update

--- run.sh
+++ run.sh
@@ -5,5 +5,9 @@ export PATH=$PATH:~/node-v8.5.0-linux-x64/bin
 
 mkdir -p "/tmp/Testude 365/agent"
 echo $PWD
+APPID=$1
+APPSECRET=$2
+echo $APPID
+echo $APPSECRET
 
-java -jar "$PWD/agent/versions/1.0.0/pc-agent.jar" "$PWD/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" 0
+java -jar "$PWD/agent/versions/1.0.0/pc-agent.jar" "$PWD/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" "0" "$APPID" "$APPSECRET"
