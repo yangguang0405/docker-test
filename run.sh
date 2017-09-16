fdc2e5a55e4804215f8efc38bd535b741a0210ea
yangguang0405 <yangguang0405@live.com>
2017-09-16 17:48:28 +0800

    Update

--- run.sh
+++ run.sh
@@ -11,4 +11,4 @@ echo $APPID
 echo $APPSECRET
 echo $CONTAINER_ID
 
-java -jar "Docker" "$PWD/agent/versions/1.0.0/pc-agent.jar" "Console" "$PWD/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" "0" "$APPID" "$APPSECRET" "$CONTAINER_ID"
+java -jar "$PWD/agent/versions/1.0.0/pc-agent.jar" "Docker" "$PWD/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" "0" "$APPID" "$APPSECRET" "$CONTAINER_ID"
