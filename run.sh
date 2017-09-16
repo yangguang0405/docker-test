2b1d6380fb33bfb7444dcfe321a99df4d9109f45
yangguang0405 <yangguang0405@live.com>
2017-09-16 17:34:01 +0800

    update

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ ls -l
 export PATH=$PATH:~/node-v8.5.0-linux-x64/bin
 
 mkdir -p "/tmp/Testude 365/agent"
+
+CONTAINER_ID=$(cat /etc/hostname)
 echo $PWD
 echo $APPID
-echo $APPSECRECT
+echo $APPSECRET
+echo $CONTAINER_ID
 
-java -jar "$PWD/agent/versions/1.0.0/pc-agent.jar" "Console" "$PWD/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" "0" "$APPID" "$APPSECRECT"
+java -jar "Docker" "$PWD/agent/versions/1.0.0/pc-agent.jar" "Console" "$PWD/agent" "/tmp/Testude 365/agent" "1.0.0" "https://www.testude365.com:8081" "https://www.testude365.com:8082/v1" "0" "$APPID" "$APPSECRET" "$CONTAINER_ID"
